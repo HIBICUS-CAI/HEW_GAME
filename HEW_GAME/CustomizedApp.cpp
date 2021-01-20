81469671ae3e5837c8c74c4ce038883b75bc2d20
caigenkan <cai_genkan@outlook.com>
2021-01-20 18:48:12 +0900

    redo temp changing

--- HEW_GAME/CustomizedApp.cpp
+++ HEW_GAME/CustomizedApp.cpp
@@ -3,17 +3,9 @@
 #include "SceneManager.h"
 #include "SpriteAnimator.h"
 
-
-//---------------------
-SPRITE_ANIME g_TestSprite;
-int g_TestOffset = 0;
-
 void AppInit()
 {
     InitSceneNodes();
-
-
-    g_TestSprite = CreateSpriteAnimator(3, "AsciiDraw", POSITION_2D(5, 5), 15, 13);
 }
 
 void AppUpdate()
@@ -21,8 +13,6 @@ void AppUpdate()
     //UpdateCurrScene();
 
     //DrawScene(GetManagedCurrScene());
-
-    DrawSpriteAnimatorToUpdateBuffer(&g_TestSprite, (g_TestOffset++) % 60);
 }
 
 void AppTurnOff()
