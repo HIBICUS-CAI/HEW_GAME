7a37ed13c0f08693a3fcf2210140215265dd7fc2
caigenkan <cai_genkan@outlook.com>
2021-01-26 14:44:57 +0900

    updated dev log file

--- HEW_GAME/AppTitleScene.h
+++ HEW_GAME/AppTitleScene.h
@@ -17,18 +17,6 @@ void InitTitleScene()
 void UpdateTitleScene()
 {
     ClearSceneCamBuffer(GetSceneNodeByName("title"));
-
-    int width = GetSceneNodeByName("title")->GetCamAddr()->CameraWidth;
-    int height = GetSceneNodeByName("title")->GetCamAddr()->CameraHeight;
-    char* cam = GetSceneNodeByName("title")->GetCamAddr()->GetCamBuffer();
-
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < width; j++)
-        {
-            *(cam + i * width + j) = '.';
-        }
-    }
 }
 
 void TurnOffTitleScene()
