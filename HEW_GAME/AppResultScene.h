5106fcdc435784f3eeb2b085f33fe226306ab604
caigenkan <cai_genkan@outlook.com>
2021-02-15 13:12:59 +0900

    add rank SA with temp code to show it

--- HEW_GAME/AppResultScene.h
+++ HEW_GAME/AppResultScene.h
@@ -7,6 +7,12 @@
 
 #include "SceneNode.h"
 
+//------------------
+#include "SpriteAnimator.h"
+#include "GameAppStructs.h"
+SPRITE_ANIME g_s;
+int g_ttime;
+
 void InitResultScene()
 {
     CreateSceneNode(SCENENODE("result", NULL,
@@ -15,11 +21,24 @@ void InitResultScene()
     GetSceneNodeByName("result")->SetBaseUIO(
         GetUIObjByName("final-result"));
     ClearSceneCamBuffer(GetSceneNodeByName("result"));
+
+    //--------------------
+    g_s = CreateSpriteAnimator(
+        60, "Assets\\SpriteAnimators\\result\\C",
+        POSITION_2D(0, 0), 70, 35
+    );
+    g_ttime = 0;
 }
 
 void UpdateResultScene()
 {
     ClearSceneCamBuffer(GetSceneNodeByName("result"));
+
+    //---------------------
+    DrawSpriteAnimatorToCamBuffer(
+        GetSceneNodeByName("result")->GetCamAddr(),
+        &g_s, (g_ttime++) % 60, POSITION_2D(0, 0)
+    );
 }
 
 void TurnOffResultScene()
